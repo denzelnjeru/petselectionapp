0e7d49a9d37de4a07213b6827b3aff2779d60e27
denzelnjeru <147034066+denzelnjeru@users.noreply.github.com>
2023-10-26 12:08:28 +0300

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,21 @@
+
+package assignment;
+public class Assignment {
+    public static void main(String[] args) {
+    int sp, bp, profit, rate;
+    if (250000>=500000){
+    rate=150%;
+    }
+    else if(500001>=1500000){
+        rate=190.26%
+    }
+    else if(1500001>=200000){
+        rate=213.98%
+    }
+    else if(>=2000001);
+    }
+    System.out.printf("%. 3f is the profit", profit);
+}  
+
+    
+}
